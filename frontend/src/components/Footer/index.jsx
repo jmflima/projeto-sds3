576365beb4708daa4e9575cf9c976f0e61fec0f4
Joao Lima <jmflima@gmail.com>
2021-05-06 21:07:45 -0300

    Static charts

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container">
-        <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Nelio Alves</a></p>
+        <p className="text-light">App desenvolvido por João Lima<a href="https://github.com/jmflima/projeto-sds3" target="_blank" rel="noreferrer">    João Lima</a></p>
         <p className="text-light"><small><strong>Semana Spring React</strong><br/>
           Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
       </div>
